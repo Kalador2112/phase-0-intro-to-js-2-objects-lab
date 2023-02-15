8b9bfe49de5cb32ce10b7059cceb77735caa0083
Kadin Caldwell <kcwell000@gmail.com>
2023-02-14 19:42:01 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: `Sam`,
+    streetAddress: '11 Broadway',
+  };
+
+ function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...employee}
+    newEmployee[key] = value
+    return newEmployee
+ }
+
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value;
+    return employee;
+ }
+
+
+ function deleteFromEmployeeByKey(obj, key) {
+    const copyEmployee = {...employee}
+    delete copyEmployee.name
+    return copyEmployee
+ }
+
+
+ function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee.name
+    return employee
+ }
